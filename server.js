e5da091a788a1f04e5b615cbbc9abfad4c6fa7de
muhammadgriffin <mohamed.bkever@gmail.com>
2022-06-01 13:06:14 -0400

    added dockerfile

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 console.log("Hello from log")
 
-express = require('express')
+const express = require('express')
+
+const app = express()
+
+const PORT = 3000
+
+const HOST = '0.0.0.0'
+
+app.get('/',(req,res)=>{
+    res.send("Hello Ensias\n")
+})
+
+app.listen(PORT,HOST)
+
+console.log(`running on ${HOST}:${PORT} `)
